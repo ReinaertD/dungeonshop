6511e73adee35e53f90499d813a8d9251216b8eb
waiz4GitHub <reinaert.depourcq@gmail.com>
2020-08-10 13:47:11 +0200

    added vuerouter and routes

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Main from "../views/Main.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "iMain",
+    component: Main,
+  },
+  {
+    path: "/form",
+    name: "Form",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Form.vue"),
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
---
+++ src/views/Form.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="Form">
+    <h1>This is the checkout page</h1>
+  </div>
+</template>
